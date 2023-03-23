03efee636b906049707680a94376e440c9d86b32
Abin-k-Rajan <abin27577@gmail.com>
2023-03-23 23:23:31 +0530

    adding functionalities

--- src/common/Cards/ListTicket.js
+++ src/common/Cards/ListTicket.js
@@ -1,12 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {IoIosAirplane} from 'react-icons/io'
 import {TbPointFilled} from 'react-icons/tb'
+import { api_url } from "../../common";
 
-function ListTicket() {
+function ListTicket({plane_id, passengers, seats}) {
+
+    const [fromStation, setFromStation] = useState('')
+    const [toStaiton, setToStation] = useState('')
+    const [departure, setDeparture] = useState(0)
+    const [departureMins, setDepartureMins] = useState(0)
+    const [arrival, setArrival] = useState(0)
+    const [arrivalMins, setArrivalMins] = useState(0)
+
+    const [departueDate, setDepartureDate] = useState()
+    const [arrivalDate, setArrivalDate] = useState()
+
+    const [hrs, setHrs] = useState()
+    const [mins, setMins] = useState()
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/flights/get-flight-detail?id=641c17fd7395c320aca21cf1`).then(res => {
+        const from_url=`${api_url}/airport/get-airport-details?id=${res.data[0].from}`
+        const to_url=`${api_url}/airport/get-airport-details?id=${res.data[0].to}`
+
+        axios.get(from_url).then(res => setFromStation(res.data[0]))
+        axios.get(to_url).then(res => setToStation(res.data[0]))
+
+        const departure_date = new Date(res.data[0].departure)
+        const arrival_date = new Date(res.data[0].arrival)
+
+        setDepartureDate(departure_date.toDateString())
+        setArrivalDate(arrival_date.toDateString())
+
+        setDeparture(departure_date.getHours())
+        setDepartureMins(departure_date.getMinutes())
+        setArrival(arrival_date.getHours())
+        setArrivalMins(arrival_date.getMinutes())
+
+        setMins(res.data[0].duration % 60)
+        setHrs(parseInt(res.data[0].duration / 60))
+
+    })
+    }, [])
     return (
         <>
-        <div className='flightroute-container container'>
-                <div className='grid grid-flow-col grid-cols-[10%_35%_20%_35%]'>
+        <div className='p-10 container'>
+        <div className='grid grid-flow-col grid-cols-[10%_35%_20%_35%]'>
                     <div className='m-auto'>
                         <div className='grid place-content-center'>
                             <div className='m-auto'>
@@ -16,16 +56,16 @@ function ListTicket() {
                         </div>
                     </div>
                     <div className='px-5'>
-                        <div className='small-text'>31 Mar Fri 2023</div>
-                        <div className='large-text'>BLR <span className='bold'>16:15</span></div>
-                        <div className='small-text'>Bengaluru International Airport</div>
+                        <div className='small-text'>{departueDate}</div>
+                        <div className='large-text'>{fromStation.iata_code} <span className='bold'>{departure}:{`${departureMins/10 < 1 ? '0' : ''}`}<span>{departureMins}</span></span></div>
+                        <div className='small-text'>{fromStation.name}</div>
                     </div>
                     <div className='text-center'>
                         <div className='flex'>
                             <div className='m-auto'>
                                 <TbPointFilled className='logo' />
                             </div>
-                            <div className='large-text bold'>1h 15m</div>
+                            <div className='large-text bold'>{hrs}h {mins}m</div>
                             <div className='m-auto'>
                                 <IoIosAirplane className='logo' />
                             </div>
@@ -33,10 +73,20 @@ function ListTicket() {
                         <div className='small-text'>Flight Duration</div>
                     </div>
                     <div className='px-5'>
-                        <div className='small-text'>31 Mar Fri 2023</div>
-                        <div className='large-text'>MAA <span className='bold'>17:15</span></div>
-                        <div className='small-text'>Chennai International Airport</div>
+                        <div className='small-text'>{arrivalDate}</div>
+                        <div className='large-text'>{toStaiton.iata_code} <span className='bold'>{arrival}:{`${arrivalMins/10 < 1 ? '0' : ''}`}<span>{departureMins}</span></span></div>
+                        <div className='small-text'>{toStaiton.name}</div>
                     </div>
+                </div>
+                <div>
+                    {
+                        passengers.map((val, index) => (
+                            <div className="flex place-content-evenly m-5 text-md bold">
+                                <div>{`Passenger ${index + 1} : ${val}`}</div>
+                                <div>{`Seat Number : ${seats[index]}`}</div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </>
--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,10 +1,19 @@
-import React, { Fragment } from "react";
+import axios from "axios";
+import React, { Fragment, useEffect, useState } from "react";
+import { api_url } from "../../common";
 import ListTicket from "../../common/Cards/ListTicket";
 import FlightRoute from "../../common/FlightRoute/FlightRoute";
 import './userprofile.scss'
 
 
 function UserProfile () {
+    const [tickets, setTickets] = useState([])
+
+    useEffect(() => {
+        const user_id = localStorage.getItem('user_id')
+        axios.get(`${api_url}/user/get-tickets?user_id=${user_id}`).then(res => setTickets(res.data))
+    }, [])
+
     return (
         <Fragment>
             <div className="h-[80vh] py-10">
@@ -16,10 +25,11 @@ function UserProfile () {
                         Download your tickets from here
                     </div>
                     <div>
-                        <ListTicket />
-                        <ListTicket />
-                        <ListTicket />
-                        <ListTicket />
+                        {
+                            tickets.map((val, index) => (
+                                <ListTicket plane_id={val.flight_route_id} passengers={val.passengers} seats={val.seats}/>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
